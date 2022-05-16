d0355b444a0796125e91638547b5bdf1619b84c5
Lusun1 <lusunbupt@gmail.com>
2022-05-16 02:02:38 -0700

    added inference path

--- scripts/run_inference.sh
+++ scripts/run_inference.sh
@@ -1,5 +1,5 @@
 python /home/slu/Projects/metaGenerator/fairseq/examples/bart/summarize.py \
-  --model-dir /content/checkpoints \
+  --model-dir /home/slu/Projects/metaGenerator/checkpoints \
   --model-file checkpoint_best.pt \
   --src /home/slu/Projects/metaGenerator/preprocess/metareview/test.source \
   --out /home/slu/Projects/metaGenerator/preprocess/metareview/test.hypo
