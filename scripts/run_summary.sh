c15c0eb4c6e5ac29dc6830306228f16076047582
Lusun1 <lusunbupt@gmail.com>
2022-05-20 12:16:16 -0700

    added new data

--- scripts/run_summary.sh
+++ scripts/run_summary.sh
@@ -1,5 +1,5 @@
 python /home/slu/Projects/metaGenerator/fairseq/examples/bart/summarize.py \
   --model-dir /home/slu/Projects/metaGenerator/checkpoints \
   --model-file checkpoint_best.pt \
-  --src /home/slu/Projects/metaGenerator/preprocess/metareview/test.source \
-  --out /home/slu/Projects/metaGenerator/preprocess/metareview/test.hypo
+  --src /home/slu/Projects/metaGenerator/preprocess/metareview/test1.source \
+  --out /home/slu/Projects/metaGenerator/preprocess/metareview/test1.hypo
