eddc8cdf487f29bec966a74fe993e3d33e545c5e
Lusun1 <lusunbupt@gmail.com>
2023-04-04 16:06:03 -0700

    code to be added

--- scripts/run_summary.sh
+++
@@ -1,5 +0,0 @@
-python /home/slu/Projects/metaGenerator/fairseq/examples/bart/summarize.py \
-  --model-dir /home/slu/Projects/metaGenerator/checkpoints \
-  --model-file checkpoint_best.pt \
-  --src /home/slu/Projects/metaGenerator/preprocess/metareview/test1.source \
-  --out /home/slu/Projects/metaGenerator/preprocess/metareview/test1.hypo
